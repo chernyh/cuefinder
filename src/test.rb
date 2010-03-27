509ee5e9e87cda6b318f34a299925df1a803d12c
chernyh <chernyh@chernyh-note.(none)>
2010-03-27 20:13:14 +0600

    new tests

--- src/test.rb
+++ src/test.rb
@@ -1,6 +1,7 @@
+#!/usr/bin/ruby
+require "cflib"
+
 def testParseGDJBRelease
-
-
   file_name="Markus Schulz - Global DJ Broadcast (2010-03-11) [PS].mp3"
   release_no=file_name.scan /Global DJ Broadcast \((.*)\).*\.mp3/
   puts "release_no=#{release_no[0][0]}"
@@ -23,5 +24,24 @@ puts "cue_url_release_pattern=#{cue_url_release_pattern}"
   puts "scanned url: #{url}"
 end
 
-testParseGDJB
-#testParseGDJBRelease
+def testParseGDJBRelease2
+file_name = "Markus Schulz presents - Global DJ Broadcast WMC Special (25 March 2010).mp3"
+cf=MarkusShultzParser.new( file_name )
+rel_no=cf.parse_release_no()
+puts "releaseNo='#{rel_no}'"
+
+end
+
+def testCueUrlFromHtmlPage
+  html= <<ENDOF_URL
+<a href="download.php?type=cue&amp;folder=gdjb&amp;filename=01-markus_schulz_-_global_dj_broadcast_wmc_special-%28di.fm%29-net-25-03-2010-mjm.cue"><img src="layout/download.png" alt="Download!" /></a> <a href="?page=tracklist&amp;folder=gdjb&amp;filename=01-markus_schulz_-_global_dj_broadcast_wmc_special-%28di.fm%29-net-25-03-2010-mjm.cue"> Global DJ Broadcast (2010-03-25) (WMC 2010 Edition) [PS/MJM]</a><br /> 
+<a href="download.php?type=cue&amp;folder=gdjb&amp;filename=01-Markus_Schulz_-_Global_DJ_Broadcast-NET-2010-03-25-iRUSH.cue"><img src="layout/download.png" alt="Download!" /></a> <a href="?page=tracklist&amp;folder=gdjb&amp;filename=01-Markus_Schulz_-_Global_DJ_Broadcast-NET-2010-03-25-iRUSH.cue"> Global DJ Broadcast (2010-03-25) (WMC 2010 Edition) [iRUSH]</a><br />
+ENDOF_URL
+
+  cf=MarkusShultzParser.new( "" )
+cf.parse_url_to_cue_file(html,"25-03-2010")
+
+end
+
+
+system("mp3splt -c cue.cue /mnt/multimedia/_RADIOSHOW_/Global\\ DJ\\ Broadcast\\ -\\ Markus\\ Schulz/2010-03-25/Markus\\ Schulz\\ presents\\ -\\ Global\\ DJ\\ Broadcast\\ WMC\\ Special\\ \\(25\\ March\\ 2010\\).mp3")
